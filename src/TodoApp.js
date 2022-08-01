2077e56b29c21cc3aa77d812fcd148b4819120db
yangjie-dev <353361719@qq.com>
2022-08-01 23:55:35 +0800

    add react router

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router";
 import { TodoList } from "./features/todos/components/todoList/TodoList";
+import { BrowserRouter, Link } from "react-router-dom";
+import HelpPage from "./features/todos/components/help/HelpPage";
+import "./TodoApp.css"
 
 export default function TodoApp() {
-  return <TodoList></TodoList>;
+  return (
+    <div>
+      <BrowserRouter>
+      <Link className = "link" to='/'> Home </Link>
+      <Link className = "link" to='/help'> Help </Link>
+        <Routes>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/help" element={<HelpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
